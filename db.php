2c018b078f839d2e329ecb5a6b1905c6b187e112
Maksym B <1maxbogdanov@gmail.com>
2025-10-03 17:56:17 +0300

    Fixed database connection error

--- db.php
+++ db.php
@@ -1,5 +1,7 @@
 <?php
 
+use mysqli_sql_exception;
+
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -7,12 +9,18 @@ $dbname = "sewing_orders";
 
 $conn = null;
 
-$temp_conn = @new mysqli($host, $user, $pass, $dbname);
+try {
+    $temp_conn = new mysqli($host, $user, $pass, $dbname);
 
+    if ($temp_conn->connect_error) {
+        $conn = null;
+        $temp_conn->close();
+    } else {
 
-if ($temp_conn && !$temp_conn->connect_error) {
-    $temp_conn->set_charset("utf8mb4");
-    $conn = $temp_conn;
-} else {
+        $temp_conn->set_charset("utf8mb4");
+        $conn = $temp_conn;
+    }
+} catch (mysqli_sql_exception $e) {
+    error_log("DB Connection failed on remote host: " . $e->getMessage());
     $conn = null;
 }
